94e3e143fd8bbf6b8a761e49e9e628c93c9e952e
Judgewooden <douwe.jong@gmail.com>
2015-11-14 14:34:42 +0100

    Make raw data available in JSON

--- Arduino_email/Arduino_email.ino
+++ Arduino_email/Arduino_email.ino
@@ -1,14 +1,18 @@
 #include "Ethernet.h"
 #include <SPI.h>
 
-int pin_input = 2;
+int pin_input = 3;
 int pin_output = 4;
 
 // Ethernet library configuration
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //physical mac address
-byte ip[] = { 192, 168, 8, 11 }; // ip in lan
-byte gateway[] = { 192, 168, 8, 1 }; // internet access via router
-byte smtp_server[] = { 192, 168, 8, 5}; // douwedejong.com
+byte mac[] = { 
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //physical mac address
+byte ip[] = { 
+  192, 168, 8, 11 }; // ip in lan
+byte gateway[] = { 
+  192, 168, 8, 1 }; // internet access via router
+byte smtp_server[] = { 
+  192, 168, 8, 5}; // douwedejong.com
 
 // HMTL processing variables
 EthernetServer server(80); //server port
@@ -21,9 +25,10 @@ char data[50];
 char datamotor[5];
 
 // new code
-int debugflow = 0;
+int debugflow = 1;
 int jsonresponse = 0;
 int lastpulse = 0;
+int Is_Power_On = 0;
 unsigned long last_pulse_time = 0;
 unsigned long current_time;
 unsigned long report_count = 0;
@@ -32,8 +37,8 @@ unsigned long timeout_millisecs = 5000;
 const double MilliPerSecond = 1000;
 double Pulses_Per_Second;
 double Liter_per_minute;
-int Is_Power_On = 0;
-long override_time= 120000;  // Overrude sgub moniutoring fail 
+double dummy_since_there_seem_to_be_ram_problems_here;
+long override_time= 120000;  // Give a 2 minute startup --- since a human will be in the room
 int override_mode = 0;
 
 // Keep the last 10 seconds time stamps
@@ -42,7 +47,7 @@ unsigned long readings[numReadings];
 int index = 0;
 
 void setup() {
-    
+
   pinMode(pin_input, INPUT);
   pinMode(pin_output, OUTPUT);
   //attachInterrupt(0, blink, FALLING);
@@ -56,6 +61,9 @@ void setup() {
 
   digitalWrite(pin_output, HIGH);
   Is_Power_On = 1;
+
+  Serial.print("POWER:");
+  Serial.println(Is_Power_On);
 
   if(override_time>0)
     override_mode=1;
@@ -71,6 +79,7 @@ void loop() {
 
   // read the input pin:
   int buttonState = digitalRead(pin_input);
+  Serial.print(buttonState);
 
   // count pulses in the loop
   if ( buttonState != lastpulse ) {
@@ -94,8 +103,10 @@ void loop() {
     if (debugflow) Serial.print(timeout_millisecs);
     if (debugflow) Serial.print(" milliseconds. ");
     if (override_mode) {
-      if (debugflow) Serial.println("KILL SIGNAL SKIPPED)!");
-    }
+      if (debugflow) {
+        Serial.println("KILL SIGNAL SKIPPED)!");
+      }
+    } 
     else {
       digitalWrite(pin_output, LOW);
       if (debugflow) Serial.println("SEND KILL SIGNAL");
@@ -106,12 +117,12 @@ void loop() {
     }
   }
 
-// ---- Hmmmmmmm this is not needed !!!!
-// if (current_time==10000) {
-//     Serial.println("sent email");   
-//     sendmail();
-//  }
-  
+  // ---- Hmmmmmmm this is not needed !!!!
+  // if (current_time==10000) {
+  //     Serial.println("sent email");   
+  //     sendmail();
+  //  }
+
   // report some interersting things
   if( (current_time / report_interval) > report_count) {
     if (debugflow) Serial.print("Time: ");
@@ -128,15 +139,19 @@ void loop() {
       total += readings[looper] - readings[looper - 1];    
 
     for ( looper = index + 1; looper < numReadings; looper++)
-      total += readings[looper] - readings[looper -1];     
+      total += readings[looper] - readings[looper - 1];     
 
     Pulses_Per_Second = MilliPerSecond / (total / numReadings);
+
     if (debugflow) Serial.print("Average : ");
     if (debugflow) Serial.println(Pulses_Per_Second);
 
     Liter_per_minute = Pulses_Per_Second * 60 / 169;
     if (debugflow) Serial.print("Liter /pm: ");
     if (debugflow) Serial.println(Liter_per_minute);
+
+    if (debugflow) Serial.print("POWER:");
+    if (debugflow) Serial.println(Is_Power_On);
 
     // countdown the time for override mode
     if(override_mode) {
@@ -158,15 +173,15 @@ void loop() {
 }
 
 void sendmail() {
-  
+
   EthernetClient SMTPclient;
-    
+
   if(!SMTPclient.connect(smtp_server, 25))
   {
     Serial.println("connection failed.");
     return;
   }
-    
+
   Serial.println("Email Connected.");
   delay(10000);
 
@@ -185,11 +200,11 @@ void sendmail() {
   SMTPclient.println("quit");
 
   delay(5000);
-    
+
   Serial.println("Disconnecting.");
   SMTPclient.stop();
 }
- 
+
 
 void reset_http_vals() {
   // Reinitializing variables
@@ -318,28 +333,42 @@ void handle_http() {
       }
 
       if (jsonresponse) {
-          // JSON CODE
-          
+        // JSON CODE
+
         client.println("HTTP/1.1 200 OK");
         client.println("Content-Type: application/json");
         client.println("Connection: close");
         client.println();
         client.print("{\"Power\":");
-          client.print(Is_Power_On);
+        client.print(Is_Power_On);
         client.print(",\"FlowPerSecond\":");
-          client.print(Pulses_Per_Second);
+        client.print(Pulses_Per_Second);
         client.print(",\"LitersPerSecond\":");
-          client.print(Liter_per_minute);
+        client.print(Liter_per_minute);
         client.print(",\"OverrideTime\":");
-          client.print(override_time);
+        client.print(override_time);
         client.print(",\"CurrentTime\":");
-          client.print(current_time);
-        client.println("}");
+        client.print(current_time);
+        client.print(",\"data\":[");
+
+        int looper;          
+        for ( looper = index; looper < numReadings; looper++) {
+          client.print(readings[looper]);    
+          client.print(",");
+        }
+
+        for ( looper = 0; looper < index; looper++) {
+          client.print(readings[looper]);    
+          if (looper < (index-1))
+            client.print(",");
+        }
+
+        client.println("]}");
         client.println("");
 
-          
-      } else {
-          // HTML CODE
+      } 
+      else {
+        // HTML CODE
         client.println("<!DOCTYPE html>");
         client.print("<meta http-equiv=\"refresh\" content=\"");
         client.print(report_interval/1000*5);
@@ -351,13 +380,13 @@ void handle_http() {
           client.print("The power is ON");
         else
           client.print("The power is OFF");
-  
+
         client.print("<br>Flow per second: ");
         client.print(Pulses_Per_Second);
-  
+
         client.print("<br>Liters per minute: ");
         client.print(Liter_per_minute);
-  
+
         if (override_time>0) {
           client.print("<br>Override mode, time remaining: ");
           client.print(override_time / 1000 / 60);
@@ -371,7 +400,7 @@ void handle_http() {
         else 
           client.print("<br>Monitoring mode");
         client.print("<br> ");
-  
+
         client.println("<br><form name=input method=post>Set override minutes : ");
         client.println("<input type=\"text\" value=\"");
         //     client.print(override_time);
@@ -379,7 +408,7 @@ void handle_http() {
         client.print(" (0=off,150=max) ");
         client.println("<input type=\"submit\" value=\"Reset\" />");                               
         client.println("</form> <br /> ");
-  
+
         client.println("</html>");        
       }
       client.stop();
@@ -388,6 +417,7 @@ void handle_http() {
 
   reset_http_vals();
 }
+
 
 
 
